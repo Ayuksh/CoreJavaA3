ac368f455689ef8d0932bd26daf1367a16296d8c
Ayush <ayuksh05@gmail.com>
2023-08-05 16:44:37 +0530

    parametrize method

---
+++ Circle.java
@@ -0,0 +1,14 @@
+class Circle
+{
+	public static void area( int r )
+	{
+		double area = 3.14 * r * r ;
+		System.out.println("AREA OF CIRCLE : "+ area);
+	}
+	
+	public static void main(String[] args)
+	{
+		area( 10 );
+		area( 10.99 );	
+	}
+}
---
+++ Rectangle.java
@@ -0,0 +1,37 @@
+class Rectangle
+{
+	public static void area()
+	{
+		int len = 10 ;
+		int wid = 5 ;
+		int area = len * wid ;
+		System.out.println("AREA : "+ area );
+	}
+
+	public static void perimeter()
+	{
+		int len = 10 ;
+		int wid = 5 ;
+		int peri = 2 * (len + wid);
+		System.out.println("PERIMETER "+ peri);
+	}
+	
+	public static void main(String[] args)
+	{
+		area();
+		perimeter();
+	}
+}
+		
+
+
+
+
+
+ 
+
+
+
+
+
+
