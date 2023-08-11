6a66d0f734c6d46b69605b8ab3a6728db5d21f53
Ayush <ayuksh05@gmail.com>
2023-08-11 16:53:44 +0530

    if example

---
+++ AgeCalculator.java
@@ -0,0 +1,42 @@
+import java.util.Scanner ;
+
+class AgeCalculator
+{
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("ENTER AGE OF 3 PEOPLE !!");
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int c = sc.nextInt();
+		
+
+		if(a>b)
+		{
+			if(a>c)
+				System.out.println("A IS OLDEST , WHOSE AGE IS "+ a);
+			else
+				System.out.println("C IS OLDEST , WHOSE AGE IS "+ c);
+		}else if(b>c){
+			System.out.println("B IS OLDEST , WHOSE AGE IS "+ b);
+			}else{
+				System.out.println("C IS OLDEST , WHOSE AGE IS "+ c);
+			}
+	}
+}
+				
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
