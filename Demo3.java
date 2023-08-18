cecd20ffdec10017a102184f0ad7c1e02cc109de
Ayush <ayuksh05@gmail.com>
2023-08-18 16:21:31 +0530

    user define constructor

---
+++ Demo3.java
@@ -0,0 +1,11 @@
+class Demo3
+{
+	public static void main(String[] args)
+	{
+		System.out.println("MAIN METHOD !!");
+	}
+
+	static {
+		System.out.println("static block ");
+		}
+}
---
+++ MainApp8.java
@@ -0,0 +1,52 @@
+class Product
+{
+	int productId ;
+	String productName ; 
+	double productPrice ;
+	String productMaf ;
+	String productExp ;
+	String productType ;
+	
+	Product( int id , String name , double price , String maf , String exp , String type )
+	{
+		System.out.println("CONSTRUCTOR EXECUTING !!");
+		productId = id ;
+		productName = name ;
+		productPrice = price ;
+		productMaf = maf ;
+		productExp = exp ;
+		productType = type ;
+	}
+
+	public void displayDetails()
+	{
+		System.out.println("---------------------------");
+		System.out.println("ID "+ productId );
+		System.out.println("NAME "+ productName );
+		System.out.println("PRICE  "+ productPrice );
+		System.out.println("MAF "+ productMaf );
+		System.out.println("EXP "+ productExp );
+		System.out.println("TYPE "+ productType );
+	}
+}
+
+class MainApp8
+{
+	public static void main(String[] args)
+	{
+		Product p1 = new Product(101 , "TATA SALT" , 20 , "09-2022" , "09-2024" , "GROCERY");
+		Product p2 = new Product(102 , "AMUL  MILK" , 50  , "18-2023" , "19-2023" , "GROCERY");
+		
+		if(p1.productId == 102)
+			p1.productPrice = 40 ;
+
+		p1.displayDetails();
+		p2.displayDetails();
+	}
+}
+		
+
+
+
+
+
