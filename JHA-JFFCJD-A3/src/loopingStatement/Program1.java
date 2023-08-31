d29c1e9e0b0b005c99bf4e237d922c9be7832faf
Ayush <ayuksh05@gmail.com>
2023-08-31 16:57:03 +0530

    looping statement

--- JHA-JFFCJD-A3/src/loopingStatement/Program1.java
+++ JHA-JFFCJD-A3/src/loopingStatement/Program1.java
@@ -1,26 +1,16 @@
 package loopingStatement;
 
+
+import java.util.Scanner;
+
 public class Program1 {
     public static void main(String[] args) {
-        int line = 5 ;
-        int star = 1 ;
-        int space = 2 ;
-       for (int j =1 ; j<=line ; j++) {
-
-           for (int k =1 ; k <= space ; k++)
-            System.out.print(" ");
-
-
-           for (int i = 1; i <= star; i++) {
-               System.out.print("*");
-           }
-           System.out.println();
-           if (j <3)
-           {
-               star+=2 ; space--;
-           }else {
-               star-=2 ; space++;
-           }
-       }
+        Scanner sc = new Scanner(System.in);
+        int sum = 0 ;
+        for (int i =50 ; i<=100 ; i++)
+        {
+            sum+=i;     // sum = sum+i ;
+        }
+        System.out.println(sum);
     }
 }
